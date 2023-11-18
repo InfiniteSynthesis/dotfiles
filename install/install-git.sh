0623eabf7bf1f1b6e60436a929ac3a2fe6629cc4
Kanade Kenzaki <kanade.kenzaki@gmail.com>
2023-11-18 16:14:20 +0000

    Fix(git): Add link for user-config
    Co-authored-by: InfiniteSynthesis <shenyu.tcv@gmail.com>

--- install/install-git.sh
+++ install/install-git.sh
@@ -2,6 +2,11 @@
 
 mkdir -p "$XDG_CONFIG_HOME/git"
 
-ln -sf "$DOTFILES/git/diff-so-fancy" "/$XDG_CONFIG_HOME/git"
+ln -sf "$DOTFILES/git/diff-so-fancy" "$XDG_CONFIG_HOME/git"
 ln -sf "$DOTFILES/git/gitconfig" "$XDG_CONFIG_HOME/git/config"
 ln -sf "$DOTFILES/git/gitignore" "$XDG_CONFIG_HOME/git/ignore"
+
+if [ -f "$DOTFILES/git/user-config" ]
+then
+	ln -sf "$DOTFILES/git/user-config" "$XDG_CONFIG_HOME/git/user-config"
+fi
